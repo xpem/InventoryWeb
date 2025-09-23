e736a455ac10c87b1ca22e048f0b877043d046b1
emanuel <emanuel.martins@hotmart.com>
2025-09-23 18:27:49 -0300

    Atualiza validações e adiciona novas propriedades
    - Adicionada validação `[MaxLength(249)]` à propriedade `Name`.
    - Nova propriedade opcional `TechnicalDescription` com `[MaxLength(349)]`.
    - `PurchaseValue` agora é opcional, aceita `0` e possui:
      - Validação de formato com `[RegularExpression]`.
      - Limite de caracteres com `[StringLength(11)]`.
    - Nova propriedade opcional `PurchaseStore` com `[MaxLength(99)]`.
    - Nova propriedade opcional `ResaleValue` com validações:
      - Formato com `[RegularExpression]`.
      - Limite de caracteres com `[StringLength(11)]`.
    - Nova propriedade opcional `Comment` com `[MaxLength(349)]`.

--- Models/UIRequests/UIItem.cs
+++ Models/UIRequests/UIItem.cs
@@ -14,22 +14,31 @@ namespace Models.UIRequests
         public int? Id { get; set; }
 
         [Required(ErrorMessage = "O campo Nome é obrigatório")]
+        [MaxLength(249, ErrorMessage = "O campo Nome deve ter no máximo 249 caracteres")]
         public required string Name { get; set; }
 
+        [MaxLength(349, ErrorMessage = "O campo Nome deve ter no máximo 349 caracteres")]
         public string? TechnicalDescription { get; set; }
 
         public DateOnly AcquisitionDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
 
         public AcquisitionType? AcquisitionType { get; set; }
 
-        public string PurchaseValue { get; set; }
+        // PurchaseValue agora é opcional e aceita 0
+        [RegularExpression(@"^(0|([1-9]\d{0,7}(\.\d{1,2})?))$", ErrorMessage = "O campo Valor de Compra deve ser um número decimal positivo com até 8 dígitos inteiros e 2 casas decimais, podendo ser 0")]
+        [StringLength(11, ErrorMessage = "O campo Valor de Compra deve ter no máximo 11 caracteres")]
+        public string? PurchaseValue { get; set; }
 
+        [MaxLength(99, ErrorMessage = "O campo Nome deve ter no máximo 99 caracteres")]
         public string? PurchaseStore { get; set; }
 
+        [RegularExpression(@"^(0|([1-9]\d{0,7}(\.\d{1,2})?))$", ErrorMessage = "O campo Valor de Compra deve ser um número decimal positivo com até 8 dígitos inteiros e 2 casas decimais, podendo ser 0")]
+        [StringLength(11, ErrorMessage = "O campo Valor de Compra deve ter no máximo 11 caracteres")]
         public string? ResaleValue { get; set; }
 
         public ItemSituation? Situation { get; set; }
 
+        [MaxLength(349, ErrorMessage = "O campo Nome deve ter no máximo 349 caracteres")]
         public string? Comment { get; set; }
 
         public CategoryDTO? Category { get; set; }
